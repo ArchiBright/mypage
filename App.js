e865becbd98c4b237144608d6b39bd009b0c32c6
ArchiArchi <archi50235@gmail.com>
2024-07-23 16:00:39 -0700

    ready

---
+++ App.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className="App">
+        <Navbar />
+        <Header />
+        <Technologies />
+        <FrontEndApps />
+        <FullStackApps />
+        <OtherProjects />
+        <Footer />
+    </div>
+  )
+}
+
+export default App
